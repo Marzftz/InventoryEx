57b2100cd2530bd51bf69507cf0f0455fe553db5
Marcela F <marce_fuertes@hotmail.com>
2020-06-30 18:20:50 -0500

    Cambiar contraseña

--- src/routes/controller_contrasena.js
+++ src/routes/controller_contrasena.js
@@ -4,3 +4,33 @@ const router = expres.Router();
 const path = require('path');
 
 const db = require('../database');
+
+
+//-------Cambio de contrseña---------//
+
+router.post("/validatecontra", async (req, res) => {
+    const { email, password } = req.body;
+
+      //Validamos que los datos no vengan vacios
+  if (email.length || password.length !== 0) {
+
+        //Realizamos consulta con las credenciales del usuario
+        const queryemail = await db.query(
+            `SELECT * FROM usuario WHERE UsuCorreo_Electronico = '${email}' `
+      
+
+  }else{
+          //Si las variables son vacias se direcciona a la pantalla de login
+    var textalerta = "Los datos son obligatorios";
+    var alerta = "alerta_visible";
+    //Redireccionamos a la url y enviamos mensaje de alerta y nombre de la clase css
+    res.render("layouts/index", { textalerta, alerta });
+  }
+
+  }
+
+
+
+
+
+}
